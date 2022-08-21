fe1e45f6eb017cb6899864a6b7184e0d84eba76f
Eslam10497 <eslam.wael104@gmail.com>
2022-08-21 22:33:51 +0200

    build: Note array render

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import sampleNotes from "./notes";
 
 function App() {
   return (
     <>
       <Header />
-      <Note />
+      {sampleNotes.map((note) => {
+        return <Note key={note.key} title={note.title} content={note.content} />;
+      })}
       <Footer />
     </>
   );
