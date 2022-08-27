863102c8ee43324c7d7a5da8159a3d70839473f1
Eslam10497 <eslam.wael104@gmail.com>
2022-08-27 17:53:01 +0200

    build: add note function skeleton

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import sampleNotes from "./notes";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+  const [addNote, setAddNote] = useState("false");
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
   return (
     <>
       <Header />
+      <div className="note">
+        <h1>
+          <input
+            value={note.title}
+            onChange={(event) => {
+              const title = event.target.value;
+              setNote({ title: title });
+            }}
+          />
+        </h1>
+        <p>
+          <input
+            value={note.content}
+            onChange={(event) => {
+              const content = event.target.value;
+              setNote({ content: content });
+            }}
+          />
+        </p>
+      </div>
       {sampleNotes.map((note) => {
         return <Note key={note.key} title={note.title} content={note.content} />;
       })}
