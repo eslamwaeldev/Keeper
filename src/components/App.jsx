eada594b879c569f08fb8c95e16c1591c015528e
Eslam10497 <eslam.wael104@gmail.com>
2022-08-29 13:05:01 +0200

    refactor: move InsertNote to a separate component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,44 +3,20 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import sampleNotes from "./notes";
+import InsertNote from "./InsertNote";
 
 function App() {
   const [notes, setNotes] = useState([]);
   const [addNote, setAddNote] = useState(false);
-  const [note, setNote] = useState({
-    title: "",
-    content: "",
-  });
+  function isReadyToAdd() {
+    setAddNote(true);
+  }
+
   return (
     <>
       <Header />
-      <div className="flex-container">
-        <div className="insert-note">
-          <h1>
-            <input
-              placeholder="Title"
-              value={note.title}
-              onChange={(event) => {
-                const title = event.target.value;
-                setNote({ title: title });
-                setAddNote(true);
-              }}
-            />
-          </h1>
-          <p>
-            <input
-              placeholder="Content"
-              value={note.content}
-              onChange={(event) => {
-                const content = event.target.value;
-                setNote({ content: content });
-                setAddNote(true);
-              }}
-            />
-          </p>
-          {addNote && <button>Add</button>}
-        </div>
-      </div>
+      <InsertNote isAddNote={isReadyToAdd} addNote={addNote} />
+
       <div className="grid-container">
         {sampleNotes.map((note, index) => {
           return (
